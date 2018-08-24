65d5d8660bf9fc25af24dbc96d5aa5a38d6ff43d
BELHADJ KACEM <sbelhadjkacem@bluexml.com>
2018-08-24 16:10:53 +0200

    feat:(index.js) using winston to handel errors who can potentially crache the server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+Joi.objectId = require('joi-objectid')(Joi);
+const winston = require('winston');
 const config = require('config');
 const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
 
 const genres = require('./routes/genres');
@@ -14,14 +15,27 @@ const rentals = require('./routes/rental');
 const express = require('express');
 const app = express();
 
+// handling errors
+winston.handleException(
+  new winston.transports.File({ filename: 'uncaughtExceptions.log'}));
+
+process.on('unhandledRejection',(ex) => {
+  throw ex;
+});
+
+// get jwtPrivateKey from variable environement
 if (!config.get('jwtPrivateKey')) {
   console.error('FATAL ERROR: jwtPrivateKey is not defined.');
   process.exit(1);
 }
+
+// connecte to mongoDB
 mongoose.connect('mongodb://localhost:27017/vidly')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'));
 
+
+// give hand to handel route
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
